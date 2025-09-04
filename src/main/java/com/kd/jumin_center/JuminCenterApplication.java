38f6f554cfe002fa9460c163ac2956707c71a70d
myammi51 <gakusei250206@gmail.com>
2025-09-04 09:57:00 +0900

    first commit

---
+++ src/main/java/com/kd/jumin_center/JuminCenterApplication.java
@@ -0,0 +1,13 @@
+package com.kd.jumin_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuminCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuminCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kd/jumin_center/JuminCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kd.jumin_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuminCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
