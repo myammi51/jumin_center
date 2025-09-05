c27b635b551c841662855f42f1d37c2d227e0a28
myammi51 <gakusei250206@gmail.com>
2025-09-05 17:40:18 +0900

    마이페이지 (민원확인 페이지)

--- src/main/java/com/kd/jumin_center/controller/TestController.java
+++ src/main/java/com/kd/jumin_center/controller/TestController.java
@@ -2,12 +2,20 @@ package com.kd.jumin_center.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class TestController {
     
-    @GetMapping("/")
+    @GetMapping("/login")
     public String main() {
         return "/login";
     }
+
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "/mypage";
+    }
+    
 }
